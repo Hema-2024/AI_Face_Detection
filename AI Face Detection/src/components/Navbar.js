65da18258575149b70ea7df883609fdec3ac1a19
unknown <hemasrik2508@gmail.com>
2023-11-18 18:49:19 +0530

    My First Commit

---
+++ AI Face Detection/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Navbar = () => {
+    return (
+        <div className="navbar">
+             <span>LamaSocial</span>
+        </div>
+    )
+}
+
+export default Navbar
